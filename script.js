35de57a1517518fa47da37d8196505940d3ea9d2
magnusohrstrom <magnusohrstrom@Magnuss-MacBook-Pro-2.local>
2016-11-16 17:42:41 +0100

    hellu

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ var arrCourses = [];
 
 
 //Course constructor.
-function Course(str, veggieTrue, url){
-  this.str = str;
-  this.veggie=veggieTrue;
-  this.url = url;
+function Course(str, isVeggie, url) {
+    this.str = str;
+    this.isVeggie = isVeggie;
+    this.url = url;
 }
 //Adderar objectet Course till courseArr.
-function addCourse(course){
-  arrCourses.push(course);
+function addCourse(course) {
+    arrCourses.push(course);
 }
 //Courses objects som pushas till courseArr.
-var kolbulle = new Course("Grillad kolbulle med lingonsylt.", false,"http://www.koket.se/sommar-med-ernst/ernst-kirchsteiger/kolbulle-pa-ernsts-vis/" );
-var springroll = new Course("Vårrulle med sötsursås.", true,"http://www.ica.se/recept/farska-varrullar-med-grillad-tofu-och-ingefarsdipp-720099/");
+var kolbulle = new Course("Grillad kolbulle med lingonsylt.", false, "http://www.koket.se/sommar-med-ernst/ernst-kirchsteiger/kolbulle-pa-ernsts-vis/");
+var springroll = new Course("Vårrulle med sötsursås.", true, "http://www.ica.se/recept/farska-varrullar-med-grillad-tofu-och-ingefarsdipp-720099/");
 var haloumi = new Course("Haloumisallad", true, "http://mittkok.expressen.se/recept/halloumisallad/");
 var hamburger = new Course("Hamburgare med pommes.");
 var chicken = new Course("Grillad kyckling med rotfrukter.");
@@ -27,85 +27,79 @@ addCourse(haloumi);
 var button = document.getElementById('button');
 
 button.addEventListener("click", generateRandomCourse);
+
+
+
+
+
 //Function for button to gererate a course-log.
-function generateRandomCourse(){
-  var temporaryP = document.getElementById("temporaryP");
+function generateRandomCourse() {
   var checkBoxValue = document.getElementById('checkbox').checked;
-  if(temporaryP === null){
+  var p = document.getElementById('courseP');
+  if (checkBoxValue === false) {
+    var i = Math.floor(Math.random() * arrCourses.length);
 
-    if(checkBoxValue===false)
-      {
-          removeElement();
-          createElement();
-
-      }
-    else
-      {
-        /*
-        while()
-        for
-        if(arrCourses[i].veggieTrue)  */
-      }
-
+    p.innerHTML = arrCourses[i].str;
   }
-  else{
-    if(checkBoxValue===false)
-      {
+  else if(checkBoxValue === true){
 
-        createElement();
+    for(var j = Math.floor(Math.random() * arrCourses.length);j<arrCourses.length;j++){
+            if(arrCourses[j].isVeggie){
+              p.innerHTML = arrCourses[j].str;
+            }
+            break;
+    /*
+var loop = true;
+    while(loop){
+      var j = Math.floor(Math.random() * arrCourses.length);
+      if(arrCourses[j].isVeggie){
+        p.innerHTML = arrCourses[j].str;
       }
-    else
-      {
-        /*
-        while()
-        for
-        if(arrCourses[i].veggieTrue)  */
-      }
+      loop=false;*/
+
+
+    }
+
+/*
+
+        }*/
+
+
+
+
+
 
   }
 }
+
+
+    //"Regular alternative"
+
+    //Veggie alternative.
+    /*
+else if(checkBoxValue) {
+      console.log("hej");*/
+
+
+      /*
+
+      var j = Math.floor(Math.random() * arrCourses.length);
+      var loop = true;
+      for(j in arrCourses){
+        if(arrCourses[j].veggieTrue===true){
+          var k = document.getElementById('courseP');
+          k.innerHTML = arrCourses[j].str;
+
+        }
+      */
+
+
+
+
 //Function that creates element for food course.
-function createElement(){
-  var i = Math.floor(Math.random()*arrCourses.length);
-  var p = document.createElement('p');
-  p.setAttribute('id',"temporaryP");
-  var courseText = arrCourses[i].str;
-  var courseTextNode = document.createTextNode(courseText);
-  p.appendChild(courseTextNode);
-  document.getElementById('mainDiv').appendChild(p);
-}
-
-//Function that removes current Foodcourse <p>.
-function removeElement(){
-  if()
-  document.getElementById('temporaryP').parentNode.removeChild(this);
-}
-
-
-  /*
-
-  }
-  else{
-    temporaryP.parentNode.removeChild(temporaryP);
-
-  }
-
-  temporaryP.parentNode.removeChild(temporaryP);
-
-
-
-  */
-
-
-  /*
-  if
-
-
-
-  if(arrCourses[index].veggie)
-    {
-      create
-    }  */
-
-
-    //var textNode=document.createTextNode(arrCourses[index]);
+/*
+function createElement() {
+    var i = Math.floor(Math.random() * arrCourses.length);
+    var p = document.getElementById('courseP');
+    p.innerHTML = arrCourses[i].str;
+}*/
